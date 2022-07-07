952cb357fa132f4c0c2cc79c0ec6447d9c4e7ae0
rainawan <rainawan02@gmail.com>
2022-07-06 22:22:29 -0700

    add scroll

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useRef } from "react";
-import { Text, View, StyleSheet, Image, Animated, PanResponder } from "react-native";
+import { Text, View, StyleSheet, Image, Animated, PanResponder, 
+  TouchableOpacity, SafeAreaView, ScrollView, StatusBar } from "react-native";
+
 
 export default function App() {
 
@@ -23,7 +25,7 @@ export default function App() {
         <Image source={require("./assets/perry.png")} style={styles.photo} />
       </View>
       <View style={styles.nameContainer}>
-        <Text style={styles.nameAndPronouns}>Raina A. Wan</Text>
+        <Text style={styles.nameAndPronouns}>Raina Alison Wan</Text>
       </View>
       <View style={styles.blurbContainer}>
         <Text style={styles.blurb}>bawk</Text>
@@ -41,6 +43,28 @@ export default function App() {
           </View>
         </Animated.View>
       </View>
+
+      <TouchableOpacity onPress={() => alert('jk')} style={styles.button}>
+        <Text style={styles.buttonText}>click me i dont work</Text>
+      </TouchableOpacity>
+
+    <SafeAreaView style={styles.scroll}>
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.scrollText}>
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+          scroll scroll scroll scroll scroll scroll scroll scroll scroll scroll 
+        </Text>
+      </ScrollView>
+    </SafeAreaView>
+
+
 
     </View>
   );
@@ -106,5 +130,33 @@ const styles = StyleSheet.create({
   wholeBox: {
     justifyContent: "center",
     alignItems: "center",
-  }
+  },
+
+  //button
+  button: {
+    backgroundColor: "#ef4c60",
+    padding: 20,
+    borderRadius: 5,
+    margin: 30,
+    marginHorizontal: 150,
+  },
+  buttonText: {
+    textAlign: "center",
+    fontSize: 15,
+    color: '#fff',
+  }, 
+
+  //sectioned scroll
+  scroll: {
+    flex: 2,
+    paddingBottom: 30,
+    paddingTop: StatusBar.currentHeight,
+    marginHorizontal: 220,
+    marginLeft: 550,
+    marginRight: 30,
+  },
+  scrollView: {
+    backgroundColor: 'pink',
+    marginHorizontal: 90,
+  },
 });
