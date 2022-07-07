9c1545451bed43ca5fc74d24c48ce39131b9ddbc
rainawan <rainawan02@gmail.com>
2022-07-06 20:48:06 -0700

    drag me button

--- App.js
+++ App.js
@@ -1,7 +1,21 @@
-import * as React from "react";
-import { Text, View, StyleSheet, Image } from "react-native";
+import React, { useRef } from "react";
+import { Text, View, StyleSheet, Image, Animated, PanResponder } from "react-native";
 
 export default function App() {
+
+  const pan = useRef(new Animated.ValueXY()).current;
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderMove: Animated.event([
+        null,
+        { dx: pan.x, dy: pan.y }
+      ]),
+      onPanResponderRelease: () => {
+        Animated.spring(pan, { toValue: { x: 0, y: 0 } }).start();
+      }
+    })
+  ).current;
 
   return (
     <View style={styles.container}>
@@ -14,6 +28,20 @@ export default function App() {
       <View style={styles.blurbContainer}>
         <Text style={styles.blurb}>bawk</Text>
       </View>
+
+      <View style = {styles.wholeBox}>
+        <Animated.View
+          style={{
+            transform: [{ translateX: pan.x }, { translateY: pan.y }]
+          }}
+          {...panResponder.panHandlers}
+        >
+          <View style={styles.box}>
+            <Text style={styles.boxText}>Drag me!</Text>
+          </View>
+        </Animated.View>
+      </View>
+
     </View>
   );
 }
@@ -21,12 +49,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#F5EDDC",
   },
   photoContainer: {
     flex: 3,
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fdad5c",
     padding: 30,
   },
   nameContainer: {
@@ -47,8 +75,36 @@ const styles = StyleSheet.create({
   nameAndPronouns: {
     fontSize: 40,
     fontWeight: "bold",
+    color: "#EB1D36",
   },
   blurb: {
     fontSize: 20,
+    color: "#A2B5BB",
   },
+
+  //drag and release box
+  titleText: {
+    fontSize: 14,
+    lineHeight: 24,
+    fontWeight: "bold",
+  },
+  box: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: 150,
+    width: 150,
+    backgroundColor: "#A2B5BB",
+    borderRadius: 5,
+    margin: 30,
+  },
+  boxText: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff",
+    padding: 20,
+  },
+  wholeBox: {
+    justifyContent: "center",
+    alignItems: "center",
+  }
 });
